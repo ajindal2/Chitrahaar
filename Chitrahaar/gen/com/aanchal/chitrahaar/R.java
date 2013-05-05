3ce50aa09c1208bafad1ba7c1901bc80e1601a7a
Aanchal Jindal <aanchaljindal@gmail.com>
2013-05-05 15:06:00 -0400

    Next button working

--- Chitrahaar/gen/com/aanchal/chitrahaar/R.java
+++ Chitrahaar/gen/com/aanchal/chitrahaar/R.java
@@ -22,9 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int images=0x7f020001;
+        public static final int next=0x7f020002;
     }
     public static final class id {
-        public static final int menu_save=0x7f080003;
+        public static final int next_song=0x7f080003;
         public static final int player_fragment=0x7f080001;
         public static final int tutorial_text=0x7f080002;
         public static final int view_container=0x7f080000;
@@ -40,7 +42,7 @@ public final class R {
         public static final int app_name=0x7f050001;
         public static final int error_player=0x7f050005;
         public static final int hello_world=0x7f050003;
-        public static final int menu_save=0x7f050000;
+        public static final int next_song=0x7f050000;
         public static final int viewVideoButton=0x7f050004;
     }
     public static final class style {
