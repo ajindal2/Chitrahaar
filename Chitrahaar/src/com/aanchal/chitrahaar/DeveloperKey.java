377d6cdba40bd82471561f0e65ba2a900701f2e4
Aanchal Jindal <aanchaljindal@gmail.com>
2013-05-04 23:14:02 -0400

    dev key removed

--- Chitrahaar/src/com/aanchal/chitrahaar/DeveloperKey.java
+++
@@ -1,5 +0,0 @@
-package com.aanchal.chitrahaar;
-
-public class DeveloperKey {
-	public static final String DEVELOPER_KEY = "AI39si5ScWdXzn9WgOB7t8DyeT704NJZotXGdIx1RCtczOwPQWIVuw2Dks3kygY18ucirOLYY3yV3pPmp-sJJsG2dXyyKnIW0A";
-}
